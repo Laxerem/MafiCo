75113be455d355534554e3b9914ffe81c6849032
Laxerem <grishaMas@yandex.ru>
2026-08-12 12:07:32 +0500

    fix(onboarding): retry profile creation on validation errors

--- MafiCo.Console/Presentation/Styles/AppComponents.cs
+++ MafiCo.Console/Presentation/Styles/AppComponents.cs
@@ -17,4 +17,12 @@ public static class AppComponents {
     public static async Task<string> GetUserInput(string title) {
         return await AnsiConsole.PromptAsync(new TextPrompt<string>(title));
     }
+    
+    public static void WriteSuccess(string text) {
+        AnsiConsole.Console.Write(new Text(text, new Style(new Color(0, 255, 0))));
+    }
+
+    public static void WriteError(string text) {
+        AnsiConsole.Console.Write(new Text(text, new Style(new Color(255, 0, 0))));
+    }
 }
--- MafiCo.Console/Presentation/Windows/Lobby/InitialWindow.cs
+++ MafiCo.Console/Presentation/Windows/Lobby/InitialWindow.cs
@@ -1,18 +1,38 @@
 using MafiCo.Console.Presentation.Base;
 using MafiCo.Console.Presentation.Extensions;
 using MafiCo.Console.Presentation.Windows.Lobby.UseCases;
+using MafiCo.Domain.SeedWork;
 using Spectre.Console;
 
 namespace MafiCo.Console.Presentation.Windows.Lobby;
 
 public class InitialWindow : Window {
     public async override Task Show() {
-        AnsiConsole.Console.Write(new FigletText("Hi!"));
-        var result = await AppComponents.GetUserInput("What is your name?");
-        await UseAsync(new CreateProfile(result));
-        AnsiConsole.Clear();
-        AnsiConsole.Console.Write(new Text("Profile created!", new Style(new Color(0, 255, 0))));
+        try {
+            await RenderAsync();
+        }
+        catch (DomainException ex) {
+            await RenderAsync(ex);
+        }
+        
         await Task.Delay(2000);
         await SwitchTo<MenuWindow>();
+    }
+
+    protected async Task RenderAsync(DomainException? error = null) {
+        AnsiConsole.Clear();
+        AnsiConsole.Console.Write(new FigletText("Hi!"));
+        if (error == null) {
+            AnsiConsole.Clear();
+            var result = await AppComponents.GetUserInput("What is your name?");
+            await UseAsync(new CreateProfile(result));
+            AppComponents.WriteSuccess("Profile Created!");
+        }
+        else {
+            AnsiConsole.Clear();
+            AppComponents.WriteError($"Error: {error.Message}");
+            var result = await AppComponents.GetUserInput("What is your name?");
+            await UseAsync(new CreateProfile(result));
+        }
     }
 }
