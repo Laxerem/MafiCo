093ec7699c4a05edd6389083b101b9f4790e4853
Laxerem <grishaMas@yandex.ru>
2026-08-13 14:31:37 +0500

    feat(profile): add method to get the current player's profile

--- MafiCo.Infrastructure/Services/ProfileService.cs
+++ MafiCo.Infrastructure/Services/ProfileService.cs
@@ -1,5 +1,7 @@
 using MafiCo.Domain.AggregatesModel.ProfileAggregate;
+using MafiCo.Domain.DTOs;
 using MafiCo.Domain.Exceptions;
+using MafiCo.Domain.SeedWork;
 using MafiCo.Infrastructure.Interfaces;
 
 namespace MafiCo.Infrastructure.Services;
@@ -13,6 +15,12 @@ public class ProfileService {
         _store = store;
         _repository = repository;
         _unitOfWork = unitOfWork;
+    }
+
+    public async Task<ProfileInfo?> GetMe() {
+        var profile = await _repository.GetAsync(_store.GetUserId()!.Value);
+        if (profile is null) return null;
+        return profile.ToProfileInfo();
     }
     
     public async Task ChangeNameAsync(string newName) {
