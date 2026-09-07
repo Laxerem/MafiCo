0e6768f106eb87b14e9e95c35409b59cf42a86e0
Laxerem <grishaMas@yandex.ru>
2026-09-07 11:34:47 +0500

    feat(game): dispatch player notifications only while processor runs

--- MafiCo.Application/Game/PlayerProcessor.cs
+++ MafiCo.Application/Game/PlayerProcessor.cs
@@ -7,16 +7,21 @@ namespace MafiCo.Application.Game;
 public class PlayerProcessor {
     public readonly PlayerContext Context;
     protected readonly IEventSource _eventSource;
+    private bool _isRunning;
 
     public PlayerProcessor(IEventSource eventEventSource) {
         Context = new PlayerContext();
         _eventSource = eventEventSource;
     }
 
-    public async Task RunAsync() {}
+    public async Task RunAsync() {
+        _isRunning = true;
+    }
 
     public void SendNotify(INotification notification) {
-        Context.AddNotification(notification);
+        if (_isRunning) {
+            Context.AddNotification(notification);
+        }
     }
 
     public void Stop() {
